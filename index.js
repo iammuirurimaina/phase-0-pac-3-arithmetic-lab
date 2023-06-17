b1b9777dded65930b62a8cc5b29bccc018e7decd
ian <ian.maina@student.moringaschool.com>
2023-06-17 08:07:14 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a = 10, b = 20 ) {
+    let sum = a + b
+    return sum
+  
+
+}
+function subtract(a, b){
+    let difference = a - b
+    return difference
+
+}
+function multiply(a, b) {
+    let multiple = a * b
+    return multiple
+
+}
+function divide(a, b) {
+    let quotient = a / b
+    return quotient
+
+}
+function increment(n){
+    let a = n++
+
+    return n
+}
+function decrement(n){
+    let a = n--
+    return n
+}
+function  makeInt(n) {
+    return n = parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    return n = parseFloat(n)
+}
+
